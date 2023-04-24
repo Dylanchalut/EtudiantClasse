aa939c431448e5d5641c81568bc8ddb359f04e7f
Dylan <redokzx1479@gmail.com>
2023-04-24 14:51:33 -0400

    version 24avril

---
+++ main2.py
@@ -0,0 +1,9 @@
+from Etudiant import *
+
+Etud_1 = Etudiant()
+Etud_1.NomEtud = "Dylan"
+Etud_1.Matricule = "1852719"
+#Etud_1.DateNaiss = "2000-04-27"
+Etud_1.Programme = "Informatique"
+#Etud_1.__str__()
+Etud_1.__dict__()
